5039ace0abd2f0142c7358832d0bc2b033c0c131
Kierra M <k.merriman32@hotmail.com>
2019-08-15 15:40:59 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,10 +1,11 @@
-def using_while(string)
-  string = 6
-	while string < 10
-	string += 1
+def using_while 
+  levitation_force = ()
+	while levitation_force < 10
+	levitation_force += 1
 puts "Wingardium Leviosa #{levitation_force}"
 end
 end
+
 
 
 
